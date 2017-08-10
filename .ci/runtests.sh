473bf704171ecf7d7415739d1db46a5315b112c2
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-08-10 13:40:26 +0200

    new command to launch MATLAB on windows

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -4,7 +4,7 @@ if [ $ARCH = "Linux" ]; then
 elif [ $ARCH = "macOS" ]; then
     /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nodesktop -nosplash < test/testAll.m
 elif [ $ARCH = "Windows" ]; then
-    C:\Program Files\Matlab\$MATLAB_VER\bin\matlab.exe"  -nodesktop -nosplash -r "run('D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\$MATLAB_VER\label\windows-biocore\test\testAll.m')
+    "C:\Program Files\Matlab\$MATLAB_VER\bin\matlab.exe"  -nodesktop -nosplash -r "run('D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\$MATLAB_VER\label\windows-biocore\test\testAll.m')"
 fi
 CODE=$?
 exit $CODE
