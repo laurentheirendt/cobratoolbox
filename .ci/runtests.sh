cbc924d0cd77dac8dcdcb59ab1f82698f8b3e03f
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-11 10:59:26 +0200

    check user id

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -9,8 +9,9 @@ elif [ "$ARCH" == "Windows" ]; then
     cd "D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore"
     # echo the current directory
     cd
+    whoami
     #cd "D:"
-    "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "initCobraToolbox;"
+    #"C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "initCobraToolbox;"
     #"run('D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore\test\testAll.m')"
     cat output.log
 fi
