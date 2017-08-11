527d339ff31de8709ffdb99edf5a9210d8c3b9f3
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-11 14:29:47 +0200

    Tail and testAll (12)

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -13,8 +13,10 @@ elif [ "$ARCH" == "Windows" ]; then
     # create a new output.log file
     #touch output.log
     "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log  -r "initCobraToolbox; exit;" #cd test; testAll;  -wait
-    tail -n0 -F  output.log 2>/dev/null | while read -t 30 line # --pid=$!
+    echo "$@"
+    tail -n0 -F --pid==$@ output.log # --pid=$!
     #cat output.log
 fi
+
 CODE=$?
 exit $CODE
