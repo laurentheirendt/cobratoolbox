4387c98f3f3e675ed3d26ee547c64ae3f46c91b0
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-11 13:40:56 +0200

    Tail and testAll

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -6,13 +6,13 @@ elif [ "$ARCH" == "macOS" ]; then
     /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nodesktop -nosplash < test/testAll.m
 elif [ "$ARCH" == "Windows" ]; then
     echo " -- Changing to the build directory --"
-    cd "D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore"
-    # echo the current directory
+    cd "D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore\test"
+    echo "User currently executing:"
     whoami
     echo " -- Launching MATLAB --"
-    "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "initCobraToolbox; exit;"
-    #"run('D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore\test\testAll.m')"
-    cat output.log
+    tail -f output.log
+    "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "testAll; exit;"
+    #cat output.log
 fi
 CODE=$?
 exit $CODE
